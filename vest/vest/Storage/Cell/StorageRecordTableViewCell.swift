fa77f7c6ccea14b5c7dc0b77671c2a33af5bcb9f
Mark Chen <chenjian345@gmail.com>
2018-03-15 20:20:32 +0800

    Not finished, will continue when I'm home

---
+++ vest/vest/Storage/Cell/StorageRecordTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  StorageRecordTableViewCell.swift
+//  VEST
+//
+//  Created by Mark on 2018/3/15.
+//  Copyright © 2018年 Mark. All rights reserved.
+//
+
+import UIKit
+
+class StorageRecordTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- vest/vest/Storage/StorageRecordListViewController.swift
+++ vest/vest/Storage/StorageRecordListViewController.swift
@@ -8,12 +8,21 @@
 
 import UIKit
 
-class StorageRecordListViewController: UIViewController {
+class StorageRecordListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableViewStorageRecords: UITableView!
+    var arrDatasource:[String]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.tabBarController?.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTapped(_:)))
+        
+        // Setup Data
+        self.arrDatasource = ["第一个单", "第2个单", "第3个单", "第4个单", "第5个单", "第6个单", "第7个单", "第8个单", "第9个单", "第14个单", "第51个单", "第16个单"]
+        
+        // Setup UI
+        self.setupTableview()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -26,7 +35,40 @@ class StorageRecordListViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: - UI Setup
+    func setupTableview() {
+        if self.tableViewStorageRecords != nil {
+            self.tableViewStorageRecords.delegate = self
+            self.tableViewStorageRecords.dataSource = self
+            
+            self.tableViewStorageRecords.register(UINib.init(nibName: "StorageRecordTableViewCell", bundle: Bundle.main), forCellReuseIdentifier: "storage_record_cell_identifier")
+        }
+    }
+    
+    // MARK: - Event handling
+    
     @objc func addTapped(_ sender: UIBarButtonItem) {
         NSLog("Add new called")
+    }
+    
+    // MARK: - UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (self.arrDatasource?.count)!
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "storage_record_cell_identifier"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        if cell != nil {
+            cell.textLabel?.text = self.arrDatasource![indexPath.row]
+        }
+        
+        return cell;
+        
+    }
+
+    // MARK: - UITableViewDelegate
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        NSLog("点击到了第\(indexPath.row)行")
     }
 }
