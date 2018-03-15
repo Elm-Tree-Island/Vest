608f85c92ddd3a44113644e571a9daa1bf562798
Mark Chen <chenjian345@gmail.com>
2018-03-15 12:55:10 +0800

    Remove unsed file and test the navigation tab bar settings

--- vest/vest/Sell/SellRecordListViewController.swift
+++ vest/vest/Sell/SellRecordListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  StorageRecordListViewController.swift
+//  SellRecordListViewController.swift
 //  VEST
 //
 //  Created by Mark on 2018/3/14.
@@ -8,25 +8,19 @@
 
 import UIKit
 
-class StorageRecordListViewController: UIViewController {
+class SellRecordListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.tabBarController?.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTapped(_:)))
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        self.tabBarController?.navigationItem.title = "Storage Record"
+        self.tabBarController?.navigationItem.title = "Sell Record"
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    @objc func addTapped(_ sender: UIBarButtonItem) {
-        NSLog("Add new called")
     }
 }
