0587578c910ac0d91f2fab702448f4756a5a89d3
Mark Chen <chenjian345@gmail.com>
2018-03-26 23:01:00 +0800

    Add new tableview part ui setup complete

--- vest/vest/Storage/MCAddStorageViewController.swift
+++ vest/vest/Storage/MCAddStorageViewController.swift
@@ -9,8 +9,9 @@
 import UIKit
 import CleanroomLogger
 
-class MCAddStorageViewController: MCBaseViewController {
+class MCAddStorageViewController: MCBaseViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var ivProductPic: UIImageView!
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,9 +19,7 @@ class MCAddStorageViewController: MCBaseViewController {
 
         self.navigationItem.rightBarButtonItem = UIBarButtonItem.init(barButtonSystemItem: .done, target: self, action: #selector(onSaveStorage))
         
-        self.ivProductPic.layer.shadowColor = UIColor(hexString: "#9B9B9B")?.cgColor
-        
-        
+        self.ivProductPic.layer.shadowColor = UIColor(hexString: "#9B9B9B")?.cgColor        
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,6 +27,10 @@ class MCAddStorageViewController: MCBaseViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: - UI Setup
+    
+    
+    // MARK: - Event Handling
     @objc func onSaveStorage() {
         // TODO: 数据格式验证 & 保存数据库
         // TEST Data
@@ -49,4 +52,52 @@ class MCAddStorageViewController: MCBaseViewController {
         self.navigationController?.popViewController(animated: true)
     }
 
+    // MARK: - UITableViewDelegate Methods
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    // MARK: - UITableViewDataSource Methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 8;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell_identifier = "add_cell_identifier_" + String("\(indexPath.row)")
+        
+        var cell = tableView.dequeueReusableCell(withIdentifier: cell_identifier)
+        if cell == nil {
+            cell = UITableViewCell(style: UITableViewCellStyle.value1, reuseIdentifier: cell_identifier)
+            switch indexPath.row {
+            case 0:
+                cell?.textLabel?.text = "名称"
+                cell?.detailTextLabel?.text = "xxx"
+            case 1:
+                cell?.textLabel?.text = "分类"
+                
+            case 2:
+                cell?.textLabel?.text = "渠道"
+                
+            case 3:
+                cell?.textLabel?.text = "进价(RMB)"
+                
+            case 4:
+                cell?.textLabel?.text = "数量"
+                
+            case 5:
+                cell?.textLabel?.text = "其他成本(RMB)"
+                
+            case 6:
+                cell?.textLabel?.text = "售价(RMB)"
+                
+            default:
+                cell?.textLabel?.text = "单品毛利(RMB)"
+            }
+            
+            cell?.accessoryType = .disclosureIndicator
+            cell?.selectionStyle = .none
+        }
+
+        return cell!
+    }
 }
