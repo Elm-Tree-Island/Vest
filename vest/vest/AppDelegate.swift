30ca30697a8af204956022ca6d7a8fb4f82af888
Mark Chen <chenjian345@gmail.com>
2018-03-31 23:32:59 +0800

    Add product detail page

--- vest/vest/AppDelegate.swift
+++ vest/vest/AppDelegate.swift
@@ -23,8 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         Log.info?.message("Application did finish launching")
-        
-        
+
         return true
     }
 
--- vest/vest/Storage/MCStorageRecordDetailViewController.swift
+++ vest/vest/Storage/MCStorageRecordDetailViewController.swift
@@ -8,13 +8,18 @@
 
 import UIKit
 
-class MCStorageRecordDetailViewController: MCBaseViewController {
+class MCStorageRecordDetailViewController: MCBaseViewController, UITableViewDelegate, UITableViewDataSource {
 
+    public var productModel:MCStorageRecordModel?
+    
+    @IBOutlet weak var ivProductPic: UIImageView!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = NSLocalizedString("Detail", comment: "")
-
-        // Do any additional setup after loading the view.
+    
+        self.tableView.tableFooterView = UIView(frame: CGRect.zero)
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,15 +27,57 @@ class MCStorageRecordDetailViewController: MCBaseViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: - UITableViewDataSource Methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if self.productModel == nil {
+            return 0
+        } else {
+            return 8
+        }
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell_identifier = "add_cell_identifier_" + String("\(indexPath.row)")
+        
+        var cell = tableView.dequeueReusableCell(withIdentifier: cell_identifier)
+        if cell == nil {
+            cell = UITableViewCell(style: UITableViewCellStyle.value1, reuseIdentifier: cell_identifier)
+            switch indexPath.row {
+            case 0:
+                cell?.textLabel?.text = "名称"
+                cell?.detailTextLabel?.text = self.productModel?.name;
+            case 1:
+                cell?.textLabel?.text = "分类"
+                
+            case 2:
+                cell?.textLabel?.text = "渠道"
+                
+            case 3:
+                cell?.textLabel?.text = "进价(RMB)"
+                cell?.detailTextLabel?.text = String.init(format: "%.2f", (self.productModel?.cost)!);
+            case 4:
+                cell?.textLabel?.text = "数量"
+                cell?.detailTextLabel?.text = "\(self.productModel?.totalCount ?? 0)"
+            case 5:
+                cell?.textLabel?.text = "其他成本(RMB)"
+                cell?.detailTextLabel?.text = String.init(format: "%.2f", (self.productModel?.otherCost)!);
+            case 6:
+                cell?.textLabel?.text = "售价(RMB)"
+                cell?.detailTextLabel?.text = String.init(format: "%.2f", (self.productModel?.price)!);
+            default:
+                cell?.textLabel?.text = "单品毛利(RMB)"
+                let totalCount:Int = Int((self.productModel?.totalCount)!)
+                var singleGrossProfit:Double = 0
+                if totalCount != 0 {
+                    let prifitSingle:Double = (self.productModel?.price)! - (self.productModel?.cost)!;
+                    singleGrossProfit = (prifitSingle * Double(totalCount) - (self.productModel?.otherCost)!) / Double(totalCount);
+                }
+                cell?.detailTextLabel?.text = "\(singleGrossProfit)"
+            }
+            
+            cell?.accessoryType = .disclosureIndicator
+        }
+        
+        return cell!
+    }
 }
