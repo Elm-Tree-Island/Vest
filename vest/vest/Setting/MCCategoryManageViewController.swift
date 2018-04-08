e823b1e2cced6d33369a6b16e43df439a21a21f2
Mark Chen <chenjian345@gmail.com>
2018-04-08 22:13:47 +0800

    Category page ui set up

--- vest/vest/Setting/MCCategoryManageViewController.swift
+++ vest/vest/Setting/MCCategoryManageViewController.swift
@@ -8,24 +8,46 @@
 
 import UIKit
 
-class MCCategoryManageViewController: MCBaseViewController {
-
+class MCCategoryManageViewController: MCBaseViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableview: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "类别管理"
         
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTapped(_:)))
         
+        self.setupTableview()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    // MARK: - UI setup
+    func setupTableview() {
+        self.tableview.register(UITableViewCell.self, forCellReuseIdentifier: "mc_setting_tableview_cell")
+        self.tableview.delegate = self
+        self.tableview.dataSource = self
+    }
 
     // MARK: - Event Handler
     @objc func addTapped(_ sender: UIBarButtonItem) {
         
     }
 
+    // MARK: - Tableview delegate
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableview.dequeueReusableCell(withIdentifier: "mc_setting_tableview_cell", for: indexPath)
+        cell.textLabel?.text = "arrRows[indexPath.row] as? String"
+        
+        cell.selectionStyle = .none
+        cell.accessoryType = .disclosureIndicator
+        
+        return cell
+    }
 }
