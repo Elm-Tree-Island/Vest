7d2078c44a3ab96c6245b7552017533cc5ac2a19
ChenJian <chenjian345@gmail.com>
2018-04-23 22:25:58 +0800

    添加渠道页面相关代码，DB部分TO BE DONE.

--- vest/vest/Setting/MCChannelManagementViewController.swift
+++ vest/vest/Setting/MCChannelManagementViewController.swift
@@ -7,11 +7,16 @@
 //
 
 import UIKit
+import CleanroomLogger
+
+let TABLEVIEW_CHANNEL_CELL_IDENTIFIER = "mc_setting_tableview_cell_channel"
 
 class MCChannelManagementViewController: MCBaseViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableview: UITableView!
 
+    var arrDataSource:NSArray!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "渠道管理"
@@ -19,6 +24,8 @@ class MCChannelManagementViewController: MCBaseViewController, UITableViewDataSo
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTapped(_:)))
         
         self.setupTableview()
+        
+//        self.arrDataSource = MCDatabaseHelper.sharedInstance.getAllCategorys()
     }
     
     override func didReceiveMemoryWarning() {
@@ -35,17 +42,54 @@ class MCChannelManagementViewController: MCBaseViewController, UITableViewDataSo
     
     // MARK: - Event Handler
     @objc func addTapped(_ sender: UIBarButtonItem) {
+        let alertController = UIAlertController(title: "", message: "", preferredStyle: .alert)
+        alertController.addTextField(configurationHandler: { (textField) in
+            textField.placeholder = "请输入货源名称"
+            
+        })
         
+        let btnCancel = UIAlertAction(title: "取消", style: .cancel, handler: nil)
+        let btnOK = UIAlertAction(title: "确定", style: .default, handler: { (action) in
+            let strInput = alertController.textFields?.first?.text
+            let model = MCChannelModel()
+            if strInput?.isEmpty == false {
+                model.name = strInput
+                
+//                let result = MCDatabaseHelper.sharedInstance.insertCategory(model:model)
+//                if (result) {
+//                    // 插入成功， 异步更新UI
+//                    self.arrDataSource = MCDatabaseHelper.sharedInstance.getAllCategorys()
+//                    DispatchQueue.main.async {
+//                        self.tableview.reloadData()
+//                    }
+//                }
+                
+            } else {
+                Log.debug?.message("名称不能为空")
+            }
+        })
+        
+        alertController.addAction(btnCancel)
+        alertController.addAction(btnOK)
+        self.present(alertController, animated: true, completion: nil)
     }
     
     // MARK: - Tableview delegate
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        if self.arrDataSource == nil {
+            return 0
+        } else {
+            return self.arrDataSource.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableview.dequeueReusableCell(withIdentifier: "mc_setting_tableview_cell", for: indexPath)
-        cell.textLabel?.text = "渠道数据123"
+        let cell = tableview.dequeueReusableCell(withIdentifier: TABLEVIEW_CHANNEL_CELL_IDENTIFIER, for: indexPath)
+        
+//        let channel:MCChannelModel = self.arrDataSource![indexPath.row] as! MCChannelModel
+//
+//        cell.textLabel?.text = channel.name
+//        cell.detailTextLabel?.text = "\(channel.channelId)"
         
         cell.selectionStyle = .none
         cell.accessoryType = .disclosureIndicator
---
+++ vest/vest/Setting/Model/MCChannelModel.swift
@@ -0,0 +1,14 @@
+//
+//  MCChannelModel.swift
+//  VEST
+//
+//  Created by markcj on 2018/4/23.
+//  Copyright © 2018 Mark. All rights reserved.
+//
+
+import UIKit
+
+class MCChannelModel: NSObject {
+    var channelId:Int64 = 0                    // ID
+    var name:String! = ""                       // 名称
+}
